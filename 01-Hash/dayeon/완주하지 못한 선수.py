7c5108588b8b0c4a2c2448944313fc7162d9eff1
HanDaYeon-coder <75533232+HanDaYeon-coder@users.noreply.github.com>
2022-02-21 09:03:32 +0900

    Delete 완주하지 못한 선수.py

--- 01-Hash/dayeon/완주하지 못한 선수.py
+++
@@ -1,8 +0,0 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    
-    for i in range(len(participant)-1) :
-        if participant[i] != completion[i] :
-            return participant[i]
-    return participant[-1]
