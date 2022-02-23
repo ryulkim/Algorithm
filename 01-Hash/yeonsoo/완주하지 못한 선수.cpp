85db557a0cb05044b43d9958c9d65d93cc501ffc
Yeonsoo So <janu10.sys@gmail.com>
2022-02-23 18:33:56 +0900

    완주하지 못한 선수(Level 1)
    https://programmers.co.kr/learn/courses/30/lessons/42576

--- 01-Hash/yeonsoo/완주하지 못한 선수.cpp
+++ 01-Hash/yeonsoo/완주하지 못한 선수.cpp
@@ -12,7 +12,7 @@ string solution(vector<string> participant, vector<string> completion) {
 	for (int i{ 0 }; i<num; i++) {
 		if (participant[i] != completion[i]) {
 			return participant[i];
-            // 사전순으로 정렬했는데 같지 않다
+            // 사전순으로 정렬했는데 같지 않다.
             // -> 참가자 리스트에만 있고 완주자 리스트에는 없는 것, 반환
 		}
 	} 
